a67418f2d66264241cd459bf891971a220e3edfa
Balasarathi29 <bala2004sarathi@gmail.com>
2025-10-31 14:57:14 +0530

    newChanges

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,15 +6,29 @@ const Quiz = () => {
   const [state, setState] = useState({
     index: 0,
     Question: Data[0],
+    lock : false
   })
-   const next = () => {
-    setState(prev => {
-      const nextIndex = prev.index + 1
-      return {
-        index: nextIndex,
-        Question: Data[nextIndex]
+  const checkAns = (e,ansIndex) => {
+    if (state.lock === false){
+      if(state.Question.answer === ansIndex + 1){
+        e.target.classList.add("correct")
+        setState(prev => ({...prev, lock: true}))
+      }else{
+        e.target.classList.add("incorrect")
+        setState(prev => ({...prev, lock: true}))
       }
-    })
+    }
+  }
+   const handleSubmit = () => {
+    setState(prev => {
+      const newIndex = prev.index + 1
+      if (newIndex >= Data.length) return prev
+      return {
+        index: newIndex,
+        Question: Data[newIndex],
+      }
+    }
+  )
   }
   return (
     <div className='container'>
@@ -23,10 +37,10 @@ const Quiz = () => {
         <h2>{state.index +1} {state.Question.question}</h2>
         <ul>
             {state.Question.options.map((opt, i) =>(
-              <li key={i}>{opt}</li>
+              <li key={i} onClick={(e) =>checkAns(e,i) }>{opt}</li>
             ))}
         </ul>
-        <button onClick={next}>Next</button>
+        <button onClick={handleSubmit}>Next</button>
         <div className='index'>{state.index + 1} of {Data.length} Questions</div>
     </div>
   )
